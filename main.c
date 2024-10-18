f252b7971da98e8d0da73beca68d47d1d2997720
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2024-10-17 22:40:49 -0600

    Added functionality for capture and transmit

--- main.c
+++ main.c
@@ -5,133 +5,187 @@
 #pragma warning disable 751
 #pragma warning disable 1498
 // define statements
-#define LED_ON 30
-#define LED_OFF 0
-
+#define IRLED_ON 30
+#define IRLED_OFF 0
+#define SAMPLE_PERIOD 13333 //sample rate close to 1200 baud for testing
+#define NUM_SAMPLES 2048
+#define NUM_BYTES = NUM_SAMPLES/8
 // function prototypes
 void myTMR1ISR(void);
-//array that we will store samples in 
-uint8_t recieve_array[256];
+void setSample(uint16_t index, uint8_t value);
+void sendSample(uint8_t recieveBuffer[], uint8_t currentByte, uint8_t currentBit);
+// array that we will store samples in
+uint8_t recieve_buffer[NUM_BYTES];
+
+//flag to control TMR1 ISR will use 1:1 prescaler
+uint8_t capture = false;
+//flag to control TMR1 ISR
+uint8_t transmit = false;
+uint8_t bufferReady = false;
+
+
 //----------------------------------------------
 // Main "function"
 //----------------------------------------------
 void main(void)
 {
-    //high i'm blanke
-   uint8_t i;
-   // initialize system
-   SYSTEM_Initialize();
-   EPWM2_LoadDutyValue(LED_OFF);
+    uint8_t i;
+    // initialize system
+    SYSTEM_Initialize();
+    // RC1 is connected to the IR LED CCP2 is the pwm used at this pin
+    EPWM2_LoadDutyValue(IRLED_OFF);
+
+    //have 
 
 
-   PIE1bits.TMR1IE = 0;
-   TMR1_SetInterruptHandler(myTMR1ISR);
-   PIE1bits.TMR1IE = 1;
+    PIE1bits.TMR1IE = 0;
+    TMR1_SetInterruptHandler(myTMR1ISR);
+    PIE1bits.TMR1IE = 1;
 
+    // allows interrupts to work:
+    INTERRUPT_PeripheralInterruptEnable();
+    INTERRUPT_GlobalInterruptEnable();
 
-   // allows interrupts to work:
-   INTERRUPT_PeripheralInterruptEnable();
-   INTERRUPT_GlobalInterruptEnable();
+    for (;;)
+    {
+        //check if holding capture button
+        if(CAPTURE_BUTTON_GetValue() == 0){
+            //button is being pressed
+            if(bufferReady == false){
+                capture = true;
+            }
+        }else{
+            capture =false;
+        }
 
-
-   for (;;)
-   {
-       
-   } // end while
+        //check if pressed transmit button has been pressed
+        if(TRANSMIT_BUTTON_GetValue() == 0){
+            transmit = true;
+        }else{
+            transmit = false;
+        }
+    } // end inf loop
 } // end main
 
 // for FSM
 typedef enum
 {
-   TX_IDLE,
-   TX_DATA_BITS,
-   TX_STOP_BITS,
-   TX_START_BITS
+    TX_IDLE,
+    TX_SAMPLE,
+    TX_DONE_SAMPLING,
+    TX_Transmit
 } tmr1ISRstate_t;
 
 //----------------------------------------------
-// My TMR1 ISR to send outgoing characters through IR transmitter
+// My TMR1 ISR to sample bits and transmit them
 // Format of the message
 // SRC DST MSG CHECKSUM NULL
 //----------------------------------------------
 void myTMR1ISR(void)
 {
+    //keep track of index in recieve buffer
+    static tmr1ISRstate_t tmr1ISRstate = TX_IDLE;
+    static uint16_t sampleIndex = 0;
+    static uint8_t bitIndex = 0;
+    static uint8_t byteIndex = 0;
+    switch (tmr1ISRstate)
+    {
+    case TX_IDLE:
+        //reset indecies for recieve buffer
+        sampleIndex = 0;
+        //if in the idle state we want to turn IR LED off
+        if (transmit == true)
+        {
+            tmr1ISRstate = TX_Transmit;// move to the next state
 
-   static uint8_t transmitBufferIndex = 0;
-   static tmr1ISRstate_t tmr1ISRstate = TX_IDLE;
-   static uint8_t tx_checksum = false;
-   static uint8_t mask;
-   static char letter;
+        } else if(capture == true){
+            tmr1ISRstate = TX_SAMPLE;// switch to sample state
+        }
+        break;
 
-   switch (tmr1ISRstate)
-   {
-   case TX_IDLE:
-       if (transmitStart == true)
-       {
-           transmitStart = false;        // turn the flag to false
-           tmr1ISRstate = TX_START_BITS; // change the state to the next state
-           transmitBufferIndex = 0;      // clear your index to make it ready
-       }
-       else
-       {
-           EPWM2_LoadDutyValue(LED_OFF); // LED_OFF so EUSART2 Rx receives 1 when it's IDLE
-       }
-       break;
+    // 
+    case TX_SAMPLE:
+        //if sampling we don't want IRLED on.
+        EPWM2_LoadDutyValue(IRLED_OFF);
+        if(capture == false){
+            //if no longer capturing go to idle state
+            tmr1ISRstate = TX_IDLE;
+        }else{
+            if(sampleIndex < NUM_SAMPLES){
+                //if capturing check the value of RCO and load into the recieve_buffer
+                setSample(sampleIndex++, PORTCbits.RCO);
+            }else{
+                //if you get past the number of samples jump to idle state
+                capture = false;
+                tmr1ISRstate = TX_DONE_SAMPLING;
+            }
+        }
+        break;
+    case TX_DONE_SAMPLING:
+        //TODO: Turn Green LED on to indicate done sampling
+        //need another flag to indicate that we don't want to overwrite data in recieve_buffer
+        bufferReady = true;
+        //clear bit and byte indexes for the transmit state
+        bitIndex = 0;
+        byteIndex = 0;
+        //return to idle state
+        tmr1ISRstate = TX_IDLE;
+        break;
 
-   // Single bit period to assert the start bit
-   case TX_START_BITS:
-       letter = transmitIRBuffer[transmitBufferIndex];
-       EPWM2_LoadDutyValue(LED_ON); // When LED is on Decoder outputs 0 as the start bit for EUSART
-       mask = 0b00000001;           // mask gets initiated
-       tmr1ISRstate = TX_DATA_BITS; // move to the next state
-       break;
+    case TX_Transmit:
+        //when we transmitting we can reasonably clear the constraint on writting to the buffer again
+        bufferReady = false;
+        if(transmit == false){
+            //transmit false go to idle state
+            tmr1ISRstate = TX_IDLE;
+        }else{
+            //transmit
+            if(byteIndex < NUM_BYTES){
+                if(bitIndex == 9){
+                    //reset the bit index
+                    //increment the byte
+                    bitIndex = 0;
+                    byteIndex++;
+                }
+                sendSample(recieve_buffer,byteIndex,bitIndex);
+                //increment the bit index
+                bitIndex++;
+            }
+        }
+        break;
 
-   // Repeat for each of the 8-bits in the letter
-   case TX_DATA_BITS:
-       if (mask == 0)
-       {
-           tmr1ISRstate = TX_STOP_BITS; // move to the next state
-       }
-       else
-       {
-           // this is the shift, &, extract thing. Refer to the 10 / 7 lecture on my website
-           if ((letter & mask) != 0)
-               EPWM2_LoadDutyValue(LED_OFF);
-           else
-               EPWM2_LoadDutyValue(LED_ON);
-           mask = mask << 1; // shift mask
-       }
-       break;
+    default:
+        tmr1ISRstate = TX_IDLE;
+        EPWM2_LoadDutyValue(IRLED_OFF);
 
-   // One bit period for stop bit
-   case TX_STOP_BITS:
-       EPWM2_LoadDutyValue(LED_OFF); // Decoder outputs 1, stop bit
-       if (transmitIRBuffer[transmitBufferIndex] == '\0')
-       {
-           // if this already reaches NULL
-           tmr1ISRstate = TX_START_BITS; // move to the n state
-           tx_checksum = true;           // turn on the tx_checksum flag so next time it enters here it'll take the checksum data in.
-       }
-       else if (tx_checksum == true)
-       {
-           tmr1ISRstate = TX_IDLE; // which state would you switch to if tx_checksum is true?
-           tx_checksum = false;    // turn off tx_checksum so it only enter here once per message
-       }
-       else
-       {
-           tmr1ISRstate = TX_START_BITS; // for normal message characters, which state should you switch to after the stop bit ?
-       }
-       transmitBufferIndex += 1; // don't forget to increment the index
-       break;
+    } // end switch
 
-   default:
-       tmr1ISRstate = TX_IDLE;
-       EPWM2_LoadDutyValue(LED_OFF);
-
-   } // end switch
-
-   TMR1_WriteTimer(0x10000 - sample_period);
-   PIR1bits.TMR1IF = 0; // clear the TMR1 interrupt flag
+    TMR1_WriteTimer(0x10000 - SAMPLE_PERIOD);//each sample will be roughly 833us
+    PIR1bits.TMR1IF = 0; // clear the TMR1 interrupt flag
 } // end Timer ISR
 
+//this code is sick!!
 
+//helper function to set the put the sample into the array
+void setSample(uint16_t index, uint8_t value){
+    uint16_t byteIndex = index / 8; // find the corresponding byte
+    uint8_t bitIndex = index % 8; // find the correspondingbit within that byte
+    //we create a unique bit mask depending on the the bit index
+    if(value ==1){
+        recieve_buffer[byteIndex] |= (1 << bitIndex); // set the bit
+    }else{
+        recieve_buffer[byteIndex] &= ~(1 << bitIndex); // clear bit
+    }
+}
+
+//helper function to transmit bit
+void sendSample(uint8_t recieveBuffer[], uint8_t currentByte, uint8_t currentBit){
+    //load PWM based on value of current bit of current byte
+    //move the bit of interest to the least significant bit position
+    uint8_t bitValue = (recieveBuffer[currentByte]>>currentBit) & 0x01;
+    if(bitValue == 1){
+        EPWM2_LoadDutyValue(IRLED_OFF);//turn LED OFF sSO DECODER GETS gets output 1
+    }else{
+        EPWM2_LoadDUtyValue(IRLED_ON);//turn LED ON SO DECODER GETS gets output 0
+    }
+}
